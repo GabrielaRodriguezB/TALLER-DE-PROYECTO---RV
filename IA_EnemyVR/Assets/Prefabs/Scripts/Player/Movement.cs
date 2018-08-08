64d6dcf5ee6828d2853b1abcaab32eff8f3833df
Angelo Fabrizio Costa Florez <30765458+angelocostausmp@users.noreply.github.com>
2018-08-08 15:44:20 -0500

    IA_Enemy_1.0
    Basic Code of an NPC Enemy

---
+++ IA_EnemyVR/Assets/Prefabs/Scripts/Player/Movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoCapsula : MonoBehaviour {
+    public float velocidad;
+    private Vector3 MoviCapsula;
+
+    public float horizontalSpeed = 2.0F;
+    public float verticalSpeed = 2.0F;
+
+    private const float Y_ANGLE_MIN = 20.0f;
+    private const float Y_ANGLE_MAX = 50.0f;
+   
+
+    // Use this for initialization
+    void Start ()
+    {
+        velocidad = 5f;
+  
+   
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+
+        float inputDerIzq = Input.GetAxis("Horizontal");
+        float inputArrAba = Input.GetAxis("Vertical");
+
+
+        float moverDerIzq = inputDerIzq * velocidad * Time.deltaTime;
+        float moverArrAba = inputArrAba * velocidad * Time.deltaTime;
+
+        transform.Translate(moverDerIzq, 0f, 0f);
+        transform.Translate(0f, 0f, moverArrAba);
+
+        
+
+    }
+}
---
+++ IA_EnemyVR/Assets/Prefabs/Scripts/Player/Movement1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+
+    public float hspeed;
+    public float vspeed;
+
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       float h = Input.GetAxis("Horizontal") * hspeed;
+       float v = Input.GetAxis("Vertical")* vspeed;
+
+        transform.Translate(h, 0, 0);
+        transform.Translate(0, 0, v);
+            
+		
+	}
+}
