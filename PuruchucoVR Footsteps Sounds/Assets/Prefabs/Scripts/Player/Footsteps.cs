b71ae3147cb13f00041325343e5a8042da627912
Angelo Fabrizio Costa Florez <30765458+angelocostausmp@users.noreply.github.com>
2018-08-16 14:30:12 -0500

    Sprint1
    Taller

---
+++ PuruchucoVR Footsteps Sounds/Assets/Prefabs/Scripts/Player/Footsteps.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour {
+
+    
+
+    // Use this for initialization
+    void Start () {
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+        /*if(Input.GetButtonDown("Vertical") || Input.GetButtonDown("Horizontal"))
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        if(Input.GetButtonUp("Vertical") || Input.GetButtonUp("Horizontal"))
+        {
+            GetComponent<AudioSource>().Stop();
+        }*/
+
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            // Only start playing, if the audio hasn't been started yet
+            if (!GetComponent<AudioSource>().isPlaying)
+                GetComponent<AudioSource>().Play();
+        }
+        else if (Input.GetAxis("Horizontal") == 0 || Input.GetAxis("Vertical") == 0)
+        GetComponent<AudioSource>().Stop();
+
+
+    }
+
+        
+
+}
