e624cbcb1047bcb834fd96b3d191420785ca2d76
Angelo Fabrizio Costa Florez <30765458+angelocostausmp@users.noreply.github.com>
2018-09-15 19:50:06 -0500

    PuruchucoVR

---
+++ Taller Jueves 06/Puruchuco v1.3.1/Assets/Prefabs/Scripts/Player/Crouch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crouch : MonoBehaviour {
+
+    CharacterController cc;
+    CapsuleCollider cap;
+    CapsuleCollider cp;
+	// Use this for initialization
+	void Start () {
+        cc = gameObject.GetComponent<CharacterController>();
+        cap = gameObject.GetComponent<CapsuleCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.C))
+        {
+            cc.height = 1f;
+        }
+        else
+        {
+            cc.height = 1.8f;
+        }
+
+	}
+}
