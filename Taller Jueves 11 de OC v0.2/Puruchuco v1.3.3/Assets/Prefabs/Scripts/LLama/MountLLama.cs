da3d6085b430f25ef9ffb1f3ba8496beb82cd6cd
Angelo Fabrizio Costa Florez <30765458+angelocostausmp@users.noreply.github.com>
2018-10-09 23:08:01 -0500

    Desmontar Llama v0.1

---
+++ Taller Jueves 11 de OC v0.2/Puruchuco v1.3.3/Assets/Prefabs/Scripts/LLama/MountLLama.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MountLLama : MonoBehaviour {
+
+    private bool useMount; 
+                           
+    public Transform mountObject; 
+    public Transform objControl;    
+    public Vector3 mountedPos;   
+    public float moveSpeed = 2f;   
+    public float mountSpeed = 3f;
+
+    public Rigidbody rgbp;
+    public Rigidbody rgbm;
+
+    private bool mounted;
+    public GameObject CameraObj;
+    public float pos = 5f;
+
+    void Start()
+    {
+        useMount = false;              
+        mounted = false;
+        objControl = transform;
+        rgbp = GetComponent<Rigidbody>();
+        
+    }
+
+
+    void Update()
+    {
+        if (useMount && Input.GetKeyDown(KeyCode.E))  
+        {
+ 
+            transform.parent = mountObject.transform;
+            rgbp = rgbm;
+            objControl = mountObject; 
+            mountedPos = mountObject.position + Vector3.up; 
+            moveSpeed += mountSpeed;
+            useMount = true;
+            transform.position = mountedPos;
+        }
+
+        if (useMount && Input.GetKeyDown(KeyCode.G))
+        {
+            
+            transform.parent = null;
+            rgbp = GetComponent<Rigidbody>();
+            objControl = transform;
+            mountSpeed -= moveSpeed;
+            useMount = false;
+
+            transform.position = new Vector3(-16.75f, -0.71f, -17.24f);
+        }
+
+
+        if (Input.GetKey(KeyCode.W) && useMount)
+        {
+            mountObject.Translate(Vector3.forward * moveSpeed * Time.deltaTime); 
+        }
+        if (Input.GetKey(KeyCode.S) && useMount)
+        {
+            mountObject.Translate(Vector3.back * moveSpeed * Time.deltaTime); 
+        }
+        if (Input.GetKey(KeyCode.A) && useMount)
+        {
+            mountObject.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D) && useMount)
+        {
+            mountObject.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
+
+        rgbp.MovePosition(objControl.position + (new Vector3(CameraObj.transform.forward.x, 0f, CameraObj.transform.forward.z) * pos * Input.GetAxis("Vertical") + new Vector3(CameraObj.transform.right.x, 0f, CameraObj.transform.right.z) * pos * Input.GetAxis("Horizontal")) * Time.deltaTime);
+
+    }
+
+    void OnGUI()
+    {
+        if (useMount) 
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 10, 100, 20), "Presiona el Boton E para subirte!");
+        }
+        
+    }
+
+    void OnTriggerEnter(Collider trigger)
+    {
+     
+        if (trigger.tag == "mount")
+        {
+            useMount = true; 
+            mountObject = trigger.transform.parent; 
+        }
+    }
+
+    void OnTriggerExit(Collider trigger)
+    {
+      
+        if (trigger.tag == "mount")
+        {
+            useMount = false; 
+            mountObject = null;
+          
+        }
+    }
+}
---
+++ Taller Jueves 11 de OC v0.2/Puruchuco v1.3.3/Assets/Prefabs/Scripts/Player/Movement.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    
+    public float pos = 10f;
+    public GameObject CameraObj;
+    public int forceConst = 5;
+    private bool grounded=true;
+    private bool canJump;
+    private Rigidbody selfRigidbody;
+    CapsuleCollider colliderCapsula;
+    // Use this for initialization
+    void Start () {
+        selfRigidbody = GetComponent<Rigidbody>();
+        colliderCapsula = gameObject.GetComponent<CapsuleCollider>();
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+        
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            // Only start playing, if the audio hasn't been started yet
+            if (!GetComponent<AudioSource>().isPlaying)
+                GetComponent<AudioSource>().Play();
+        }
+        else if (Input.GetAxis("Horizontal") == 0 || Input.GetAxis("Vertical") == 0)
+            GetComponent<AudioSource>().Stop();
+
+
+        if (Input.GetButton("Jump") && grounded == true)
+        {
+            canJump = true;
+        }
+
+        if(Input.GetButton("Fire1"))
+        {
+            colliderCapsula.height = 1.0f;
+        }
+        else
+        {
+            colliderCapsula.height = 1.8f;
+        }
+    }
+
+    void OnCollisionEnter(Collision hit)
+    {
+        if (hit.gameObject.tag == "grounded")
+        {
+            grounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision hit)
+    {
+        if (hit.gameObject.tag == "grounded")
+        {
+            grounded = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition(transform.position + (new Vector3(CameraObj.transform.forward.x, 0f, CameraObj.transform.forward.z) * pos * Input.GetAxis("Vertical") + new Vector3(CameraObj.transform.right.x, 0f, CameraObj.transform.right.z) * pos * Input.GetAxis("Horizontal")) * Time.deltaTime);
+
+        if (canJump)
+        {
+            canJump = false;
+            selfRigidbody.AddForce(0, forceConst, 0, ForceMode.Impulse);
+        }
+        
+    }
+    
+
+
+
+
+
+
+}
+
+
