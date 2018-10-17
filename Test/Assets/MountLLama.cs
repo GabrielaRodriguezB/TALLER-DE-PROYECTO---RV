0f7d0151485ee2e21d23e2b522d95540e3149330
LuisCP2012 <luiscp123@hotmail.com>
2018-10-17 14:23:14 -0500

    Ejemplo Cubitos

---
+++ Test/Assets/MountLLama.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MountLLama : MonoBehaviour {
+
+    public GameObject Jugador;
+    public GameObject Llama;
+    public GameObject CamaraVR;
+    public GameObject EmptyGameObject;
+    MovimientoCarrito scriptMove;
+    public bool estado = false;
+    void Start()
+    {
+        Jugador = GameObject.FindWithTag("Player");
+        scriptMove = GetComponent<MovimientoCarrito>();
+        //mounted = false;  
+    }
+
+    void Update()
+    {
+        if (estado && Input.GetKeyDown(KeyCode.E))  
+        {
+            Jugador.SetActive(false);
+            //CamaraVR.transform.parent = Llama.transform;
+            CamaraVR.transform.parent = EmptyGameObject.transform;
+            Jugador.transform.parent = Llama.transform;
+            Llama.GetComponent<MovimientoCarrito>().enabled = true;
+            scriptMove.enabled = true;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Carrito")
+        {
+            estado = true;
+        }
+    }
+}
---
+++ Test/Assets/MovimientoCarrito.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoCarrito : MonoBehaviour {
+
+    public float pos = 10f;
+    public GameObject CameraObj;
+    MovimientoCarrito script;
+    public int forceConst = 5;
+    private bool canJump;
+    private Rigidbody selfRigidbody;
+    // Use this for initialization
+    void Start()
+    {
+        selfRigidbody = GetComponent<Rigidbody>();
+        script = GetComponent<MovimientoCarrito>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition(transform.position + (new Vector3(CameraObj.transform.forward.x, 0f, CameraObj.transform.forward.z) * pos * Input.GetAxis("Vertical") + new Vector3(CameraObj.transform.right.x, 0f, CameraObj.transform.right.z) * pos * Input.GetAxis("Horizontal")) * Time.deltaTime);
+
+        if (canJump)
+        {
+            canJump = false;
+            selfRigidbody.AddForce(0, forceConst, 0, ForceMode.Impulse);
+        }
+
+    }
+}
